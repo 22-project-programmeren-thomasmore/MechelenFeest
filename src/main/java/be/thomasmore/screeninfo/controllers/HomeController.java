6f306959c3fe24fd3ab1bf58f1407f53876e882c
DennyVerschueren <r0952915@student.thomasmore.be>
2023-05-26 00:27:49 +0200

    reset password layout + language

--- src/main/java/be/thomasmore/screeninfo/controllers/HomeController.java
+++ src/main/java/be/thomasmore/screeninfo/controllers/HomeController.java
@@ -3,6 +3,8 @@ package be.thomasmore.screeninfo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -15,5 +17,15 @@ public class HomeController {
     @GetMapping("/contact")
     public String getContactPagina() {
         return "contact";
+    }
+
+    @GetMapping("/reset-password")
+    public String getResetPassword() {
+        return "reset-password";
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPasswordPost(@RequestParam String emailAddress) {
+        return "reset-password";
     }
 }
